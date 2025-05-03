88ab4515892e4eedc9da7294289d73bf4530844d
martilumastro <martina.lumastro001@studenti.uniparthenope.it>
2025-05-03 23:24:47 +0200

    new page: page all week's alarms.com:martilumastro/DreamHealthy
    Signed-off-by: martilumastro <martina.lumastro001@studenti.uniparthenope.it>

---
+++ app/src/main/java/com/example/dreamhealthy/MyAlarmsClockActivity.kt
@@ -0,0 +1,49 @@
+package com.example.dreamhealthy
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MyAlarmsClockActivity : AppCompatActivity() {
+
+    private lateinit var melodyBtn: ImageButton
+    private lateinit var todayBtn: ImageButton
+    private lateinit var menuBtn: ImageButton
+
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_my_alarms_clock)
+
+
+            //init image buttons
+        melodyBtn = findViewById(R.id.melody_bt)
+        todayBtn = findViewById(R.id.today_bt)
+        menuBtn = findViewById(R.id.menu_bt)
+
+        //call fun
+        buttonChange()
+    }
+
+    fun buttonChange() {
+        // button hamburger --> from alarms to menu
+        val buttonMenu = findViewById<ImageButton>(R.id.menu_bt)
+        buttonMenu.setOnClickListener {
+            val pageMenu = Intent(this, MenuActivity::class.java)
+            startActivity(pageMenu)
+        }
+        // button today --> from home to today
+        val buttonToday = findViewById<ImageButton>(R.id.today_bt)
+        buttonToday.setOnClickListener {
+            val pageToday = Intent(this, TodayActivity::class.java)
+            startActivity(pageToday)
+        }
+    }
+}
