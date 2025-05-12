530d88b59a9a1cba7a648bfd380aea6819928d92
martilumastro <martina.lumastro001@studenti.uniparthenope.it>
2025-05-12 16:21:47 +0200

    New main page
    Signed-off-by: martilumastro <martina.lumastro001@studenti.uniparthenope.it>

--- app/src/main/java/com/example/dreamhealthy/MainActivity.kt
+++ app/src/main/java/com/example/dreamhealthy/MainActivity.kt
@@ -2,64 +2,44 @@ package com.example.dreamhealthy
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.Menu
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.viewpager.widget.ViewPager
+import java.time.DayOfWeek
+import java.time.LocalDate
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 class MainActivity : AppCompatActivity() {
-
-    //var for view page
-    private lateinit var buttonMenu: ImageButton
-    private lateinit var buttonWeekValue: ImageButton
-    private lateinit var textButton: TextView
-    private lateinit var homeBtn: ImageButton
+    //val for find today
+    private val today = LocalDate.now()
+    private val dayOfWeek = today.dayOfWeek
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //init image buttons
-        buttonMenu = findViewById(R.id.menuBt)
-        buttonWeekValue = findViewById(R.id.week_values_bt)
-        textButton = findViewById(R.id.home_text_button)
-        homeBtn = findViewById(R.id.homeBt)
-
-
-        //call fun
-        buttonChange()
-        printTextButton()
-    }
-
-
-    //fun for all changing page
-    fun buttonChange(){
-        // button hamburger --> from home to menu
-        buttonMenu.setOnClickListener {
-            val pageMenu = Intent (this, MenuActivity::class.java)
-            startActivity(pageMenu)
+        // go every time in today page
+        when (dayOfWeek) {
+            DayOfWeek.MONDAY -> startActivity(Intent(this, TodayActivity::class.java))
+            DayOfWeek.TUESDAY -> startActivity(Intent(this, TuesdayActivity::class.java))
+            DayOfWeek.WEDNESDAY -> startActivity(Intent(this, WednesdayActivity::class.java))
+            DayOfWeek.THURSDAY -> startActivity(Intent(this, ThursdayActivity::class.java))
+            DayOfWeek.FRIDAY -> startActivity(Intent(this, FridayActivity::class.java))
+            DayOfWeek.SATURDAY -> startActivity(Intent(this, SaturdayActivity::class.java))
+            DayOfWeek.SUNDAY -> startActivity(Intent(this, SundayActivity::class.java))
         }
-        buttonWeekValue.setOnClickListener {
-            val pageWeekValue = Intent (this, WeekValuesMenuActivity::class.java)
-            startActivity(pageWeekValue)
-        }
-        // button home --> NULL
-        homeBtn.setOnClickListener {
-            val pageHome = Intent (this, MainActivity::class.java)
-            startActivity(pageHome)
-        }
-    }
 
-    //text button
-    fun printTextButton() {
-        textButton.text = "Home"
+        finish()
     }
 }
